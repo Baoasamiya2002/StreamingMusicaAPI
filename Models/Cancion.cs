501a3d5fb507a02caec378c2ab0d9abf0d8bc589
pxk <puxka_shikof@hotmail.com>
2020-07-12 17:38:44 -0500

    Agregado JsonIgnore a llaves foŕaneas

--- Models/Cancion.cs
+++ Models/Cancion.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using System.Text.Json.Serialization;
 
 namespace ApiRest.Models
 {
@@ -12,6 +13,7 @@ namespace ApiRest.Models
         public int AlbumId { set; get; }
         public int ArtistaId { set; get; }
         public int GeneroId { set; get; }
+        [JsonIgnore]
         public virtual List<CancionLista_reproduccion> CancionListas_reproduccion { set; get;}
     }
 }
--- Models/Lista_reproduccion.cs
+++ Models/Lista_reproduccion.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using System.Text.Json.Serialization;
 
 namespace ApiRest.Models
 {
@@ -10,6 +11,7 @@ namespace ApiRest.Models
         public int Id { set; get; }
         public string Nombre_lista { set; get; }
         public int UsuarioId { set; get; }
+        [JsonIgnore]
         public virtual List<CancionLista_reproduccion> CancionListas_reproduccion { set; get;}
     }
 }
